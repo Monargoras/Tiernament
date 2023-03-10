2395dd634452dea2c9a2eb3f44efa353644e20b5
Felix Wagner <felixwagner99@googlemail.com>
2023-03-10 14:43:53 +0100

    add router, add error page, remove default assets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import './App.css';
+import ErrorPage from './pages/ErrorPage';
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+    errorElement: <ErrorPage />,
+  },
+]);
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <RouterProvider router={router} />
+  )
 }
-
-export default App;
---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,18 @@
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+
+export default function ErrorPage() {
+    const error = useRouteError()
+    const isRoutErrorRes = isRouteErrorResponse(error)
+
+    return (
+        <div id="error-page" style={{textAlign: 'center'}}>
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            { isRoutErrorRes &&
+                <p>
+                    <i>{error.statusText || error.data.message}</i>
+                </p>
+            }
+        </div>
+    );
+}
