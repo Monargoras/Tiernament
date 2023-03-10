8472aa21bcc857ddbf32dddcd776adaa8c67ffee
Felix Wagner <felixwagner99@googlemail.com>
2023-03-10 17:12:58 +0100

    add cors rules

--- server/src/main/kotlin/com/tiernament/server/ServerApplication.kt
+++ server/src/main/kotlin/com/tiernament/server/ServerApplication.kt
@@ -2,11 +2,23 @@ package com.tiernament.server
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 @RestController
 class ServerApplication { }
+
+@Configuration
+class CorsConfiguration: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ServerApplication>(*args)
