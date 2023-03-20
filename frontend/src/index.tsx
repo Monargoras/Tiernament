12d9bddd90fc81cfc5961d4590884912bc706f37
Felix Wagner <felixwagner99@googlemail.com>
2023-03-20 14:21:30 +0100

    formatting

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -7,7 +7,7 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import {CssBaseline} from "@mui/material";
+import { CssBaseline } from '@mui/material';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
