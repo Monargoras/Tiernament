4aaea604983a664c7f04a2165a59deb605feabe7
Felix Wagner <felixwagner99@googlemail.com>
2023-03-22 14:17:59 +0100

    add refresh request WIP

--- frontend/src/util/requestGenerator.tsx
+++ frontend/src/util/requestGenerator.tsx
@@ -1,3 +1,6 @@
+import { updateToken } from '../redux/authSlice';
+import { useAppDispatch } from '../redux/hooks';
+
 export const backendIP = 'http://localhost:8080'
 
 export const createRequest = (method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE', apiEndpoint: string, body?: Object, pathArgument?: String) => {
@@ -22,6 +25,37 @@ export const createRequest = (method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE
       if(res.status === 401) {
         alert('Unauthorized')
       }
+      if(res.status === 403) {
+        alert('Forbidden')
+      }
       return res
+    })
+}
+
+export const createRefreshRequest = () => {
+  const dispatch = useAppDispatch()
+
+  return fetch(
+    `${backendIP}/api/user/refresh`,
+    {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+    })
+    .then((res) => {
+      if(res.status === 500) {
+        alert('Server Error')
+      }
+      if(res.status === 401) {
+        alert('Unauthorized')
+      }
+      if(res.status === 403) {
+        alert('Forbidden')
+      }
+      const authHeader = res.headers.get('Authorization')
+      if(authHeader) {
+        const token = authHeader.substring(7)
+        dispatch(updateToken(token))
+      }
     })
 }
