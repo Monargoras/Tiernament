fd083d662488967ba8888e5f1f661ccc3fd3921b
Felix Wagner <felixwagner99@googlemail.com>
2023-03-23 01:25:21 +0100

    add general styles

---
+++ frontend/src/util/styles.tsx
@@ -0,0 +1,40 @@
+export const utils = {
+  topBottomMargin: {
+    marginTop: 1,
+    marginBottom: 1,
+  },
+}
+
+export const generalStyles = {
+  backgroundContainer: {
+    minHeight: '92vh',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignContent: 'center',
+  },
+  textField: {
+    ...utils.topBottomMargin,
+  },
+  paper: {
+    padding: 8,
+    margin: 'auto',
+  },
+  button: {
+    ...utils.topBottomMargin,
+  },
+  flexWrapBox: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignContent: 'center',
+  },
+  snackbarAlert: {
+    width: '100%',
+    border: 1,
+    borderColor: '#9e9e9e'
+  },
+}
