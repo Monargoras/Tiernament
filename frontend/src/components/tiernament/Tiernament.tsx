29b3cd4521c3c4d370a9e3bc349b52bfe5c5da2f
Felix Wagner <felixwagner99@googlemail.com>
2023-03-29 13:44:43 +0200

    remove old placeholder data

--- frontend/src/components/tiernament/Tiernament.tsx
+++ frontend/src/components/tiernament/Tiernament.tsx
@@ -9,64 +9,6 @@ export async function loader(params: { tiernamentId: string }) {
   return await res.json()
 }
 
-const placeholderTiernament: TiernamentType = {
-  tiernamentId: '1',
-  authorId: '1',
-  name: 'Placeholder Tiernament',
-  description: 'Placeholder Tiernament Description',
-  date: new Date(),
-  entries: [
-    {
-      entryId: '1',
-      name: 'Fop',
-      imageLink: '',
-      placementHistory: [],
-    },
-    {
-      entryId: '2',
-      name: 'Dt',
-      imageLink: '',
-      placementHistory: [],
-    },
-    {
-      entryId: '3',
-      name: 'Mathe 1',
-      imageLink: '',
-      placementHistory: [],
-    },
-    {
-      entryId: '4',
-      name: 'Mathe 2',
-      imageLink: '',
-      placementHistory: [],
-    },
-    {
-      entryId: '5',
-      name: 'Ro',
-      imageLink: '',
-      placementHistory: [],
-    },
-    {
-      entryId: '6',
-      name: 'Afe',
-      imageLink: '',
-      placementHistory: [],
-    },
-    {
-      entryId: '7',
-      name: 'Apl',
-      imageLink: '',
-      placementHistory: [],
-    },
-    {
-      entryId: '8',
-      name: 'Aud',
-      imageLink: '',
-      placementHistory: [],
-    }
-  ]
-}
-
 export default function Tiernament() {
   const tiernament = useLoaderData() as TiernamentType;
 
