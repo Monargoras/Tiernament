3e4e9873a45f6800a5266f5e1e63ea6ba39a6958
Felix Wagner <felixwagner99@googlemail.com>
2023-04-07 16:19:44 +0200

    add tiernament card

---
+++ frontend/src/components/tiernament/TiernamentCard.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { TiernamentTitleType } from '../../util/types';
+import { useNavigate } from 'react-router-dom';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
+import { getImageLink } from '../../apiRequests/imageRequests';
+import { useTranslation } from 'react-i18next';
+import CustomAvatar from "../profile/CustomAvatar";
+
+interface TiernamentCardProps {
+  tiernament: TiernamentTitleType
+}
+
+export default function TiernamentCard(props: TiernamentCardProps) {
+
+  const navigate = useNavigate()
+  const { t } = useTranslation()
+
+  const styles = {
+    card: {
+      cursor: 'pointer',
+      margin: '10px',
+      maxWidth: 250,
+    }
+  }
+
+  const handleNavigate = () => {
+    navigate(`/tiernament/${props.tiernament.tiernamentId}`)
+  }
+
+  return (
+    <Card sx={styles.card}>
+      <CardActionArea onClick={handleNavigate}>
+        {
+          props.tiernament.imageId &&
+            <CardMedia
+                component={'img'}
+                height={'150'}
+                image={getImageLink(props.tiernament.imageId)}
+                alt={props.tiernament.name}
+            />
+        }
+        <CardContent>
+          <Typography gutterBottom variant={'h5'} component={'div'}>
+            {props.tiernament.name}
+          </Typography>
+          <Box sx={{display: 'flex', flexDirection: 'row'}}>
+            <Typography gutterBottom variant={'body1'}>
+              {t('byAuthor', {authorName: props.tiernament.authorDisplayName})}
+            </Typography>
+            <div style={{width: '5px'}} />
+            <CustomAvatar userName={props.tiernament.authorDisplayName} imageId={props.tiernament.authorAvatarId} size={{height: 25, width: 25}} />
+          </Box>
+          <Typography variant={'body2'} color={'text.secondary'}>
+            {props.tiernament.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  )
+}
