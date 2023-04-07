f6a8d6b2fd4600fefece480f099992257b579eb0
Felix Wagner <felixwagner99@googlemail.com>
2023-04-07 15:09:15 +0200

    refactor tiernament api, add service, add search tiernament endpoint

--- server/src/main/kotlin/com/tiernament/server/api/TiernamentApiController.kt
+++ server/src/main/kotlin/com/tiernament/server/api/TiernamentApiController.kt
@@ -8,40 +8,39 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.stereotype.Service
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 interface TiernamentRepo : MongoRepository<Tiernament, String> {
     fun findByTiernamentId(id: String): Tiernament?
+    fun findByAuthorId(id: String): List<Tiernament>
+    fun findByNameContainingIgnoreCase(name: String): List<Tiernament>
 }
 
-@RestController
-@RequestMapping("/api/tiernament/get")
-class PublicTiernamentApiController(@Autowired val repo: TiernamentRepo) {
-
-    @GetMapping("/count")
+@Service
+class TiernamentService(@Autowired val repo: TiernamentRepo) {
     fun getTiernamentCount(): Int {
         return repo.findAll().count()
     }
 
-    @GetMapping
     fun getTiernaments(): List<TiernamentTitleDTO> {
         return repo.findAll().map { TiernamentTitleDTO(it) }
     }
 
-    @GetMapping("/{id}")
-    fun getTiernamentById(@PathVariable("id") id: String): ResponseEntity<Tiernament> {
-        val tiernament = repo.findByTiernamentId(id)
-        return if (tiernament != null) ResponseEntity.ok(tiernament) else ResponseEntity.notFound().build()
+    fun getTiernamentsByAuthorId(id: String): List<TiernamentTitleDTO> {
+        return repo.findByAuthorId(id).map { TiernamentTitleDTO(it) }
     }
-}
 
-@RestController
-@RequestMapping("/api/tiernament/edit")
-class PrivateTiernamentApiController(@Autowired val repo: TiernamentRepo) {
+    fun getTiernamentsBySearchTerm(searchTerm: String): List<TiernamentTitleDTO> {
+        return repo.findByNameContainingIgnoreCase(searchTerm).map { TiernamentTitleDTO(it) }
+    }
 
-    @PostMapping
-    fun postTiernament(@RequestBody body: TiernamentDTO, @AuthenticationPrincipal curUser: User): Tiernament {
+    fun getTiernamentById(id: String): Tiernament? {
+        return repo.findByTiernamentId(id)
+    }
+
+    fun postTiernament(body: TiernamentDTO, curUser: User): Tiernament {
         // create uuid
         val id = UUID.randomUUID().toString()
         // create tiernament with serverside id and date
@@ -57,17 +56,66 @@ class PrivateTiernamentApiController(@Autowired val repo: TiernamentRepo) {
         return repo.insert(tiernament)
     }
 
+    fun updateTiernament(id: String, tiernament: Tiernament): Tiernament? {
+        return repo.findByTiernamentId(id = id)?.let {
+            repo.save(it.copy(name = tiernament.name, imageId = tiernament.imageId, description = tiernament.description, entries = tiernament.entries))
+        }
+    }
+
+    fun deleteTiernament(id: String) {
+        repo.findByTiernamentId(id)?.let {
+            repo.delete(it)
+        }
+    }
+}
+
+@RestController
+@RequestMapping("/api/tiernament/get")
+class PublicTiernamentApiController(@Autowired val service: TiernamentService) {
+
+    @GetMapping("/count")
+    fun getTiernamentCount(): Int {
+        return service.getTiernamentCount()
+    }
+
+    @GetMapping
+    fun getTiernaments(): List<TiernamentTitleDTO> {
+        return service.getTiernaments()
+    }
+
+    @GetMapping("/author/{id}")
+    fun getTiernamentsByAuthorId(@PathVariable("id") id: String): List<TiernamentTitleDTO> {
+        return service.getTiernamentsByAuthorId(id)
+    }
+
+    @GetMapping("/search/{searchTerm}")
+    fun getTiernamentsBySearchTerm(@PathVariable("searchTerm") name: String): List<TiernamentTitleDTO> {
+        return service.getTiernamentsBySearchTerm(name)
+    }
+
+    @GetMapping("/{id}")
+    fun getTiernamentById(@PathVariable("id") id: String): ResponseEntity<Tiernament> {
+        val tiernament = service.getTiernamentById(id)
+        return if (tiernament != null) ResponseEntity.ok(tiernament) else ResponseEntity.notFound().build()
+    }
+}
+
+@RestController
+@RequestMapping("/api/tiernament/edit")
+class PrivateTiernamentApiController(@Autowired val service: TiernamentService) {
+
+    @PostMapping
+    fun postTiernament(@RequestBody body: TiernamentDTO, @AuthenticationPrincipal curUser: User): Tiernament {
+        return service.postTiernament(body, curUser)
+    }
+
     @PatchMapping("/{id}")
     fun updateTiernament(@PathVariable("id") id: String, @RequestBody tiernament: Tiernament): Tiernament? {
-        return repo.findByTiernamentId(id = id)?.let {
-            repo.save(it.copy(name = tiernament.name, description = tiernament.description, entries = tiernament.entries))
-        }
+        return service.updateTiernament(id, tiernament)
     }
 
     @DeleteMapping("/{id}")
     fun deleteTiernament(@PathVariable("id") id: String) {
-        repo.findByTiernamentId(id)?.let {
-            repo.delete(it)
-        }
+        service.deleteTiernament(id)
     }
 }
