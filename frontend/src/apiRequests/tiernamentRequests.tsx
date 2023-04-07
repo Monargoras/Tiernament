cdecbd7926b880a50698ca22ada7b1024a4023dd
Felix Wagner <felixwagner99@googlemail.com>
2023-04-07 15:09:24 +0200

    add search request

--- frontend/src/apiRequests/tiernamentRequests.tsx
+++ frontend/src/apiRequests/tiernamentRequests.tsx
@@ -5,6 +5,10 @@ export function fetchTiernaments() {
   return createRequest('GET', 'api/tiernament/get')
 }
 
+export function fetchTiernamentsBySearchTerm(searchTerm: string) {
+  return createRequest('GET', 'api/tiernament/get/search', undefined, searchTerm)
+}
+
 export function fetchTiernamentById(tiernamentId: string) {
   return createRequest('GET', 'api/tiernament/get', undefined, tiernamentId)
 }
